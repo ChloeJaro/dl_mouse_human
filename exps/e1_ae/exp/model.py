7932da1f553f2c6368777d9c0e6cb4a1d436ba38
Mohammed Amer <mohammed.e.amer@gmail.com>
2023-06-03 16:08:36 +0100

    e1 AE model and config

---
+++ exps/e1_ae/exp/model.py
@@ -0,0 +1,157 @@
+import functools
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import lightning.pytorch as pl
+import torchmetrics
+
+
+class MLP(nn.Module):
+    def __init__(self, in_channels: int, hidden_layers: list, activation: dict, dropout: float):
+        super().__init__()
+
+        activation_clz = getattr(torch.nn, activation["name"])
+        activation_clz = functools.partial(activation_clz, **activation["init_args"])
+
+        self.mlp = []
+
+        prev_channels = in_channels
+
+        for ch in hidden_layers:
+            self.mlp.extend(
+                [
+                    nn.Linear(prev_channels, ch),
+                    activation_clz(),
+                    nn.Dropout(p=dropout),
+                ]
+            )
+
+            prev_channels = ch
+
+        self.mlp = nn.Sequential(*self.mlp)
+
+    def forward(self, x):
+        return self.mlp(x)
+
+
+class LitNet(pl.LightningModule):
+    def __init__(
+        self,
+        in_channels: int,
+        encoder_layers: list,
+        decoder_layers: list,
+        class_layers: list,
+        class_out_channels: int,
+        activation: dict,
+        dropout: float,
+        optimizer: dict,
+        loss: dict,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.train_loss = torchmetrics.MeanMetric()
+        self.val_acc = torchmetrics.Accuracy(
+            task="multiclass", num_classes=class_out_channels, average="micro"
+        )
+
+        self.encoder = MLP(
+            in_channels=in_channels,
+            hidden_layers=encoder_layers,
+            activation=activation,
+            dropout=dropout,
+        )
+
+        self.decoder = MLP(
+            in_channels=encoder_layers[-1],
+            hidden_layers=decoder_layers,
+            activation=activation,
+            dropout=dropout,
+        )
+        self.decoder = nn.Sequential(
+            self.decoder, nn.Linear(decoder_layers[-1], in_channels)
+        )
+
+        self.classifier = MLP(
+            in_channels=encoder_layers[-1],
+            hidden_layers=class_layers,
+            activation=activation,
+            dropout=dropout,
+        )
+        self.classifier = nn.Sequential(
+            self.classifier, nn.Linear(class_layers[-1], class_out_channels)
+        )
+
+    def forward(self, x):
+        latent = self.encoder(x)
+        decoder_output = self.decoder(latent)
+
+        class_output = self.classifier(latent)
+
+        return latent, decoder_output, class_output
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+
+        latent, decoder_o, class_o = self(x)
+
+        loss = 0.0
+
+        class_weight = self.hparams.loss["class_weight"]
+        reconst_weight = self.hparams.loss["reconst_weight"]
+        abs_weight = self.hparams.loss["abs_weight"]
+
+        if class_weight > 0:
+            loss += F.cross_entropy(class_o, y)
+
+        if reconst_weight > 0:
+            loss += F.mse_loss(decoder_o, x, reduction="none").sum(dim=1).mean(dim=0)
+
+        if abs_weight > 0:
+            loss += torch.abs(latent).sum(dim=1).mean(dim=0)
+
+        self.train_loss(loss, x.size(0))
+
+        return loss
+
+    def on_train_epoch_end(self):
+        loss = self.train_loss.compute()
+
+        self.log("train_loss", loss, prog_bar=True, sync_dist=True)
+
+        self.train_loss.reset()
+
+    def test_step(self, batch, batch_idx):
+        # this is the test loop
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        _, _, class_o = self(x)
+
+        self.val_acc(class_o, y)
+
+    def on_test_end(self):
+        val_acc = self.val_acc.compute()
+
+        logger = self.logger.experiment
+        logger.add_scalar("val_acc", val_acc)
+
+        self.val_acc.reset()
+
+    def predict_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+
+        return self.encoder(x)
+
+    def configure_optimizers(self):
+        opt_cfg = self.hparams.optimizer
+
+        opt_class = getattr(torch.optim, opt_cfg["name"])
+        optimizer = opt_class(
+            self.parameters(),
+            **opt_cfg["init_args"],
+        )
+        return optimizer
