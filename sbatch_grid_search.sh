0a88dbe1bd1fe8b7fe33b5e75f4533c320b58fda
Chloe Jaroszynski <uvy786@rescomp2.hpc.in.bmrc.ox.ac.uk>
2023-08-03 11:48:56 +0100

    minor changes

--- sbatch_grid_search.sh
+++ sbatch_grid_search.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -l
 
-sbatch -p gpu_short --gres gpu:1 \
-#singularity_python_run.sh \
-pipeline.sh
-exps/e1_ae/exp/tune_model.py \
-++tag=tune/run_$i \ 
+iterations=10
+
+for i in $(seq 1 "$iterations")
+    do
+    sbatch -p gpu_short --gres gpu:1 \
+    #singularity_python_run.sh \
+    pipeline.sh \
+    exps/e1_ae/exp/tune_model.py \
+    ++tag=tune/run_$i \ 
+    ++seed=$i \
+    
+    done
