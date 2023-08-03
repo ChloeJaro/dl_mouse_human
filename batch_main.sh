b44d2aa10db948d6d6e018e3d47abb4e5acdbbd1
Chloe Jaroszynski <uvy786@rescomp2.hpc.in.bmrc.ox.ac.uk>
2023-08-03 11:48:56 +0100

    minor changes

--- batch_main.sh
+++ batch_main.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -l
 
+
+iterations=10
+
+for i in $(seq 1 "$iterations")
+do
 sbatch -p gpu_short --gres gpu:1 \
 singularity_python_run.sh \
 exps/e1_ae/exp/main.py \
 ++tag=tune/run_$i \ 
+++seed=$i
+done
